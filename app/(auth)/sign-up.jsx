3f20dd27360442ed4cc1922e24f88b6ed5d3b3dd
Alek240825 <iglesiag1908@gmail.com>
2024-09-17 09:35:36 -0500

    Coneccion appwrite

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,81 +1,96 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Dimensions, Alert, Image } from "react-native";
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { images } from  '../../constants'
+import { images } from '../../constants'
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton';
 import { Link } from 'expo-router'
 
+import { createUser } from '../../lib/appwrite'
+
 const SignUp = () => {
   const [form, setForm] = useState({
-    username:'',
-    email: '',
-    password: ''
-  })
+    username: "",
+    email: "",
+    password: "",
+  });
 
-  const [isSubmitting, setisSubmitting] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
 
-  const submit = ()  => {
+  const submit = async () => {
+    if (form.username || form.email || form.password) {
+      Alert.alert("Error", "Please fill in all fields");
+    }
 
-  }
+    setIsSubmitting(true);
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
         <View className="w-full justify-center min-h-[83vh] px-4 my-6">
-        <Image 
-          source={images.logo}
-          className="w-[115px] h-[35px]"
-          resizeMode='contain'
-        />
-        <Text className="text-white text-2xl font-semibold mt-10">
-          Sign up to Aora
-        </Text>
-        <FormField 
-          title="Username"
-          value={form.username}
-          handleChangeText={(e) =>  setForm({ 
-            ...form, 
-            username: e 
-          })}
-          otherStyles="mt-10"
-        />
-        <FormField 
-          title="Email"
-          value={form.email}
-          handleChangeText={(e) =>  setForm({ 
-            ...form, 
-            email: e 
-          })}
-          otherStyles="mt-7"
-          keyboardType="email-address"
-        />
-        <FormField 
-          title="Password"
-          value={form.password}
-          handleChangeText={(e) =>  setForm({ 
-            ...form, 
-            password: e 
-          })}
-          otherStyles="mt-7"
-        />
-        <CustomButton 
-          title="Sign Up"
-          handlePress={submit}
-          containerStyles="mt-7"
-          isLoading={isSubmitting}
-        />
-        <View className="justify-center pt-5 flex-row gap-2">
-          <Text className ="text-gray-100 text-lg, font-pregular">
-            Have an account already?
+          <Image
+            source={images.logo}
+            className="w-[115px] h-[35px]"
+            resizeMode='contain'
+          />
+          <Text className="text-white text-2xl font-semibold mt-10">
+            Sign up to Aora
           </Text>
-          <Link 
-            href="/sign-in"
-            className='text-lg font-semibold text-secondary'  
-          >
-            Sign in
-          </Link>
-        </View>
+          <FormField
+            title="Username"
+            value={form.username}
+            handleChangeText={(e) => setForm({
+              ...form,
+              username: e
+            })}
+            otherStyles="mt-10"
+          />
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({
+              ...form,
+              email: e
+            })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({
+              ...form,
+              password: e
+            })}
+            otherStyles="mt-7"
+          />
+          <CustomButton
+            title="Sign Up"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-gray-100 text-lg, font-pregular">
+              Have an account already?
+            </Text>
+            <Link
+              href="/sign-in"
+              className='text-lg font-semibold text-secondary'
+            >
+              Sign in
+            </Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
